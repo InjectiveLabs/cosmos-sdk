f714514566cbdfb2910c7138a61104f1d8da61c6
Inhyeok Lee <lazka33@gmail.com>
2025-03-05 19:28:39 +0900

    chore(test): fix test types

--- store/ephemeral/cache_kv_store_test.go
+++ store/ephemeral/cache_kv_store_test.go
@@ -6,6 +6,11 @@ import (
 	"testing"
 )
 
+type testItem struct {
+	key  []byte
+	item interface{}
+}
+
 type dummyItem[T any] struct {
 	value T
 }
@@ -31,44 +36,47 @@ func calculateAdditionalSize[T any](v T) int {
 	}
 }
 
-func key(i int) []byte {
-	return []byte(fmt.Sprintf("%d", i))
+func makeTestItem(key int, value interface{}) *testItem {
+	return &testItem{
+		key:  []byte(fmt.Sprintf("%d", key)),
+		item: value,
+	}
 }
 
 func TestEphemeralCacheKVStore(t *testing.T) {
-	dummyItems := make([]interface{}, 0)
-	dummyItems = append(dummyItems, &dummyItem[int]{value: int(1)})
-	dummyItems = append(dummyItems, &dummyItem[int64]{value: int64(2)})
-	dummyItems = append(dummyItems, &dummyItem[string]{value: "3"})
-	dummyItems = append(dummyItems, &dummyItem[[]int]{value: []int{4, 5, 6}})
-	dummyItems = append(dummyItems, &dummyItem[map[string]int]{value: map[string]int{"a": 1, "b": 2}})
+	testItems := make([]*testItem, 0)
+	testItems = append(testItems, makeTestItem(0, &dummyItem[int]{value: int(1)}))
+	testItems = append(testItems, makeTestItem(1, &dummyItem[int64]{value: int64(2)}))
+	testItems = append(testItems, makeTestItem(2, &dummyItem[string]{value: "3"}))
+	testItems = append(testItems, makeTestItem(3, &dummyItem[[]int]{value: []int{4, 5, 6}}))
+	testItems = append(testItems, makeTestItem(4, &dummyItem[map[string]int]{value: map[string]int{"a": 1, "b": 2}}))
 
-	set := func(store EphemeralKVStore, dummyItems []interface{}) {
-		for i, item := range dummyItems {
-			sized, ok := item.(Sized)
+	set := func(store EphemeralKVStore, testItems []*testItem) {
+		for i := 0; i < len(testItems); i++ {
+			sized, ok := testItems[i].item.(Sized)
 			if !ok {
 				t.Fatalf("item must implement Sized interface")
 			}
-			store.Set(key(i), sized)
+			store.Set(testItems[i].key, sized)
 		}
 	}
 
-	verifyExist := func(store EphemeralKVStore, dummyItems []interface{}) {
-		for i := 0; i < len(dummyItems); i++ {
-			item := store.Get(key(i))
+	verifyExist := func(store EphemeralKVStore, testItems []*testItem) {
+		for i := 0; i < len(testItems); i++ {
+			item := store.Get(testItems[i].key)
 			if item == nil {
 				t.Fatalf("item should be in the store")
 			}
 
-			if !reflect.DeepEqual(item, dummyItems[i]) {
+			if !reflect.DeepEqual(item, testItems[i].item) {
 				t.Fatalf("item is not equal to the original item")
 			}
 		}
 	}
 
-	verifyNotExist := func(store EphemeralKVStore, dummyItems []interface{}) {
-		for i := 0; i < len(dummyItems); i++ {
-			item := store.Get(key(i))
+	verifyNotExist := func(store EphemeralKVStore, testItems []*testItem) {
+		for i := 0; i < len(testItems); i++ {
+			item := store.Get(testItems[i].key)
 			if item != nil {
 				t.Fatalf("item should not be in the store")
 			}
@@ -80,14 +88,14 @@ func TestEphemeralCacheKVStore(t *testing.T) {
 		eCacheKVStore := eCommitKVStore.Branch()
 
 		// Set
-		set(eCacheKVStore, dummyItems)
+		set(eCacheKVStore, testItems)
 
 		// Write & Commit
 		eCacheKVStore.Write()
 		eCommitKVStore.Commit()
 
 		// Get from the underlying store
-		verifyExist(eCommitKVStore, dummyItems)
+		verifyExist(eCommitKVStore, testItems)
 	})
 
 	t.Run("Branch: Drop", func(t *testing.T) {
@@ -95,10 +103,10 @@ func TestEphemeralCacheKVStore(t *testing.T) {
 		eCacheKVStore := eCommitKVStore.Branch()
 
 		// Set
-		set(eCacheKVStore, dummyItems)
+		set(eCacheKVStore, testItems)
 
 		// Get from the underlying store
-		verifyNotExist(eCommitKVStore, dummyItems)
+		verifyNotExist(eCommitKVStore, testItems)
 	})
 
 	t.Run("Branch: Write & Commit with multi layer caches", func(t *testing.T) {
@@ -107,22 +115,22 @@ func TestEphemeralCacheKVStore(t *testing.T) {
 		eCacheKVStore2 := eCacheKVStore.Branch()
 
 		// Set to cache layer 2
-		set(eCacheKVStore2, dummyItems)
+		set(eCacheKVStore2, testItems)
 
 		// Write
 		eCacheKVStore2.Write()
 
 		// Get from cache layer 1
-		verifyExist(eCacheKVStore, dummyItems)
+		verifyExist(eCacheKVStore, testItems)
 
 		// Get from EphemeralBackend
-		verifyNotExist(eCommitKVStore, dummyItems)
+		verifyNotExist(eCommitKVStore, testItems)
 
 		// Write & Commit
 		eCacheKVStore.Write()
 		eCommitKVStore.Commit()
 
 		// Get from EphemeralBackend
-		verifyExist(eCommitKVStore, dummyItems)
+		verifyExist(eCommitKVStore, testItems)
 	})
 }

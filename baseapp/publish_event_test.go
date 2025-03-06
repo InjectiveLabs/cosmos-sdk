a304533c9437984c441e110a10b228b64e299eda
Jeff <jeff.woo.dev@gmail.com>
2025-03-06 21:33:28 +0900

    feat: unit test for publish events

---
+++ baseapp/publish_event_test.go
@@ -0,0 +1,189 @@
+package baseapp_test
+
+import (
+	"fmt"
+	"testing"
+
+	"cosmossdk.io/log"
+	abci "github.com/cometbft/cometbft/abci/types"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	baseapptestutil "github.com/cosmos/cosmos-sdk/baseapp/testutil"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	types "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+var _ types.PublishEvent = (*StringPublishEvent)(nil)
+
+type StringPublishEvent struct {
+	data string
+}
+
+func (e StringPublishEvent) ToString() string {
+	return e.data
+}
+
+func (e StringPublishEvent) Serialize() []byte {
+	return []byte(e.data)
+}
+
+func getStreamEventFlushChan(app *baseapp.BaseApp) chan types.PublishEvents {
+	app.EnableStreamer = true
+	publishEventChan := make(chan types.PublishEvents)
+
+	go func() {
+		for {
+			select {
+			case e := <-app.StreamEvents:
+				if e.Flush != nil {
+					publishEventChan <- e.Flush.PublishEvents
+				}
+			}
+		}
+	}()
+
+	return publishEventChan
+}
+
+func TestPublishEvent_FinalizeBlock_WithBeginAndEndBlocker(t *testing.T) {
+	name := t.Name()
+	db := dbm.NewMemDB()
+	app := baseapp.NewBaseApp(name, log.NewTestLogger(t), db, nil)
+
+	publishEventChan := getStreamEventFlushChan(app)
+
+	app.SetBeginBlocker(func(ctx sdk.Context) (sdk.BeginBlock, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+		ctx.EventManager().EmitEvent(types.Event{
+			Type: "sometype",
+			Attributes: []abci.EventAttribute{
+				{
+					Key:   "foo",
+					Value: "bar",
+				},
+			},
+		})
+		ctx.PublishEventManager().EmitEvent(StringPublishEvent{"sometype2"})
+
+		return sdk.BeginBlock{
+			Events: ctx.EventManager().ABCIEvents(),
+		}, nil
+	})
+
+	app.SetEndBlocker(func(ctx sdk.Context) (sdk.EndBlock, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+		ctx.EventManager().EmitEvent(types.Event{
+			Type: "anothertype",
+			Attributes: []abci.EventAttribute{
+				{
+					Key:   "foo",
+					Value: "bar",
+				},
+			},
+		})
+
+		ctx.PublishEventManager().EmitEvent(StringPublishEvent{"anothertype2"})
+
+		return sdk.EndBlock{
+			Events: ctx.EventManager().ABCIEvents(),
+		}, nil
+	})
+
+	_, err := app.InitChain(
+		&abci.RequestInitChain{
+			InitialHeight: 1,
+		},
+	)
+	require.NoError(t, err)
+
+	res, err := app.FinalizeBlock(&abci.RequestFinalizeBlock{Height: 1})
+	require.NoError(t, err)
+
+	require.Len(t, res.Events, 4)
+
+	require.Equal(t, "sometype", res.Events[0].Type)
+	require.Equal(t, "foo", res.Events[0].Attributes[0].Key)
+	require.Equal(t, "bar", res.Events[0].Attributes[0].Value)
+	require.Equal(t, "mode", res.Events[0].Attributes[1].Key)
+	require.Equal(t, "BeginBlock", res.Events[0].Attributes[1].Value)
+
+	require.Equal(t, "publish event placeholder", res.Events[1].Type)
+
+	require.Equal(t, "anothertype", res.Events[2].Type)
+	require.Equal(t, "foo", res.Events[2].Attributes[0].Key)
+	require.Equal(t, "bar", res.Events[2].Attributes[0].Value)
+	require.Equal(t, "mode", res.Events[2].Attributes[1].Key)
+	require.Equal(t, "EndBlock", res.Events[2].Attributes[1].Value)
+
+	require.Equal(t, "publish event placeholder", res.Events[3].Type)
+
+	_, err = app.Commit()
+	require.NoError(t, err)
+
+	require.Equal(t, int64(1), app.LastBlockHeight())
+
+	pevts := <-publishEventChan
+	require.Len(t, pevts, 2)
+	require.Equal(t, "sometype2", pevts[0].ToString())
+	require.Equal(t, "anothertype2", pevts[1].ToString())
+}
+
+func TestPublishEvent_FinalizeBlock_DeliverTx(t *testing.T) {
+	anteKey := []byte("ante-key")
+	anteOpt := func(bapp *baseapp.BaseApp) {
+		bapp.SetAnteHandler(anteHandlerTxTestWithCustomEventEmit(t, capKey1, anteKey, true))
+	}
+	suite := NewBaseAppSuite(t, anteOpt)
+	publishEventChan := getStreamEventFlushChan(suite.baseApp)
+
+	_, err := suite.baseApp.InitChain(&abci.RequestInitChain{
+		ConsensusParams: &cmtproto.ConsensusParams{},
+	})
+	require.NoError(t, err)
+
+	deliverKey := []byte("deliver-key")
+	baseapptestutil.RegisterCounterServer(suite.baseApp.MsgServiceRouter(), CounterServerImpl{t, capKey1, deliverKey, true})
+
+	nBlocks := 3
+	txPerHeight := 5
+
+	for blockN := 0; blockN < nBlocks; blockN++ {
+
+		txs := [][]byte{}
+		for i := 0; i < txPerHeight; i++ {
+			counter := int64(blockN*txPerHeight + i)
+			tx := newTxCounter(t, suite.txConfig, counter, counter)
+
+			txBytes, err := suite.txConfig.TxEncoder()(tx)
+			require.NoError(t, err)
+
+			txs = append(txs, txBytes)
+		}
+
+		res, err := suite.baseApp.FinalizeBlock(&abci.RequestFinalizeBlock{
+			Height: int64(blockN) + 1,
+			Txs:    txs,
+		})
+		require.NoError(t, err)
+
+		for i := 0; i < txPerHeight; i++ {
+			counter := int64(blockN*txPerHeight + i)
+			require.True(t, res.TxResults[i].IsOK(), fmt.Sprintf("%v", res))
+
+			events := res.TxResults[i].GetEvents()
+			require.Len(t, events, 5, "should contain ante handler, message type and counter events respectively")
+			require.Equal(t, sdk.MarkEventsToIndex(counterEvent("ante_handler", counter).ToABCIEvents(), map[string]struct{}{})[0], events[0], "ante handler event")
+			require.Equal(t, "publish event placeholder", events[1].Type, "ante handler publish event placeholder")
+			require.Equal(t, sdk.MarkEventsToIndex(counterEvent(sdk.EventTypeMessage, counter).ToABCIEvents(), map[string]struct{}{})[0].Attributes[0], events[3].Attributes[0], "msg handler update counter event")
+			require.Equal(t, "publish event placeholder", events[4].Type, "msg handler publish event placeholder")
+		}
+
+		_, err = suite.baseApp.Commit()
+		require.NoError(t, err)
+
+		pevts := <-publishEventChan
+		require.Len(t, pevts, 2*txPerHeight)
+	}
+}
